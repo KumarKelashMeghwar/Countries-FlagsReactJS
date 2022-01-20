349b4a3177caf69d9ef5840a41e81d746e6b5b60
Kelash <58991510+KumarKelashMeghwar@users.noreply.github.com>
2022-01-20 08:24:10 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Flags from "./CountryFlags/Flags";
-// import Header from "./SimpleWebsite/header";
-// import Blog from "./SimpleWebsite/Blog";
-// import Footer from "./SimpleWebsite/footer";
+
 
 const App = () => {
   return (
     <>
       <Flags />
-
-      {/* <Header />
-      <Blog />
-      <Footer /> */}
     </>
   );
 };
